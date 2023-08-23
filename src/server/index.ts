cbcf31b4cc36f92307329b88c93bc7b873af73c2
joaoand <joao.demata@correo.unimet.edu.ve>
2023-08-22 22:09:09 -0400

    Enviar html desde el servidor y levantar el servidor

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,14 @@
-console.log("Hola mundo desde el server")
+import express, {Express, Request, Response} from "express";
+import { config } from "./config";
+import { template } from "./render/template";
+
+const app : Express = express();
+
+
+app.get('*', (_req: Request, _res: Response) => { 
+    _res.send(template(`<h1>Hola desde la ruta ${_req.url}</h1>`));
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Listening in port http://localhost:${config.PORT}`)
+});
