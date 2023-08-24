7ddcf968fc4c4a8c08eef36c0551b31b00cf1554
joaoand <joao.demata@correo.unimet.edu.ve>
2023-08-23 20:36:38 -0400

    Realizar peticion desde el servidor e inyectarla en el html y hidratacion del html

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,28 @@
 import express, {Express, Request, Response} from "express";
 import { config } from "./config";
-import { template } from "./render/template";
+import { render } from "./render";
+import axios from "axios";
 
 const app : Express = express();
 
 
+app.use(express.static('dist'));
+
+app.get('/galaxias',async (_req: Request, _res: Response) => {
+    try {
+        const {data} = await axios.get('https://images-api.nasa.gov/search?q=galaxies');
+        const initalProps = {
+            galaxies: data?.collection?.items
+        };
+
+        _res.send(render(_req.url, initalProps))
+    } catch (_error) {
+        console.error(_error)
+    }
+})
+
 app.get('*', (_req: Request, _res: Response) => { 
-    _res.send(template(`<h1>Hola desde la ruta ${_req.url}</h1>`));
+    _res.send(render('/'));
 })
 
 app.listen(config.PORT, () => {
